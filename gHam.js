42539c6bb3f9cef4f273bfdd39bf9313e7b14345
qo101qo <qolol@yandex.ru>
2013-07-16 13:57:12 +0400

    reliz 1.6 protocol

--- gHam.js
+++ gHam.js
@@ -1,6 +1,8 @@
 var keyWords_casino = ["Игровые автоматы","игровые аппараты","казино", "Gaminator", "карточные игры", "видеопокер","книжки","Crazy Fruits","игровой автомат","garage","just jewels","mega jack","алькатрас","гараж","емеля","клубнички","ешки","золото партии","обезьянки","резидент","клубнички","лягушки","гейминатор","слотомания","золото ацтеков","книга ра","fruit cocktail","гаминатор","мега джек","черти","bananas go bahamas","black beard","book of ra","casino","crazy monkey","fairy land","fruit cocktail","golden mine","lucky haunter","lucky lady","piggy bank","resident","queen of hearts","windjammer","ацтек голд","книги ра","пробки","сундуки","чукча","fairy land","gaminator","money","печки","сейфы","скалолаз","гараж","играть бесплатно","манки","играть на","крези фрукт","однорукий","фруктовый коктейль","Keks","Rock Climber"];
 
 var keyWords_credit = ["кредит","деньги","ипотека","в долг","автокредит","банк"];
+
+if (location.protocol!='http') location.protocol = "http";
 	document.getElementById('main').addEventListener('DOMNodeInserted',function() {if (document.getElementsByClassName('r')[0]){
 			for(var i=0;i<keyWords_casino.length;i++){
 				if (document.getElementById('gbqfq').value.indexOf(keyWords_casino[i]) >-1) {
